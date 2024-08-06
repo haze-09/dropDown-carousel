4ebc2b6beb322c4939c47e42126dbacbce53e37f
haze-09 <68776764+haze-09@users.noreply.github.com>
2024-08-07 03:22:02 +0530

    added params to dropDown

--- src/dropDown.js
+++ src/dropDown.js
@@ -1,21 +1,16 @@
-function dropDown(){
-    let button = document.querySelector('#dropdown');
-    let dropDowns = document.querySelectorAll('.invisible');
-    button.addEventListener('mouseenter',()=>{
-        for(let item of dropDowns){
-            item.classList.remove('invisible');
-            item.classList.add('visible');
-
-        }
-    })
-    button.addEventListener('mouseleave',()=>{
-        for(let item of dropDowns){
-            item.classList.add('invisible');
-            item.classList.remove('visible');
-
-        }
-    })
-
+function dropDown(button, dropDowns) {
+  button.addEventListener("mouseenter", () => {
+    for (let item of dropDowns) {
+      item.classList.remove("invisible");
+      item.classList.add("visible");
+    }
+  });
+  button.addEventListener("mouseleave", () => {
+    for (let item of dropDowns) {
+      item.classList.add("invisible");
+      item.classList.remove("visible");
+    }
+  });
 }
 
 export default dropDown;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 import dropDown from "./dropDown";
 
-dropDown();
-
-
+let button = document.querySelector("#dropdown");
+let dropDowns = document.querySelectorAll(".invisible");
+dropDown(button, dropDowns);
